95e54f9080dc52491436c503339efd3576e40913
Paul Stretenowich <paul.stretenowich@gmail.com>
2020-10-21 09:41:02 -0400

    Adding hoste removed metrics table

--- covid_collect_metrics.sh
+++ covid_collect_metrics.sh
@@ -6,10 +6,12 @@ module load mugqic/samtools/1.10
 
 cat /dev/null > metrics/metrics.csv
 cat /dev/null > metrics/host_contamination_metrics.tsv
+cat /dev/null > metrics/host_removed_metrics.tsv
 
 echo "sample,cons.per.N,cons.len,cons.perc.GC,cons.perc.genome_frac,cons.N_per_100_kbp,fq.trim.pass,bam.perc.align,bam.filter.pass,bam.primertrim.pass,bam.mean.cov,bam.med.cov,bam.max-min/mean.cov,bam.perc.20x,bam.perc.50x,bam.perc.100x,bam.perc.250x,bam.perc.500x,bam.perc.1000x,bam.perc.2000x,bam.mean.insertsize,bam.med.insertsize,bam.sd.insertsize,bam.min.insertsize,bam.max.insertsize" > metrics/metrics.csv
 
 echo -e "Sample\tTotal_aligned\tHuman_only\tHuman_only_perc\tSARS_only\tSARS_only_perc\tUnmapped_only\tUnmapped_only_perc" > metrics/host_contamination_metrics.tsv
+echo -e "Sample\tTotal_aligned\tHuman_only\tHuman_only_perc\tSARS_only\tSARS_only_perc\tUnmapped_only\tUnmapped_only_perc" > metrics/host_removed_metrics.tsv
 
 for sample in `awk 'NR>1 {print $1}' $readset`
 do
@@ -179,6 +181,7 @@ do
 
 
     samtools idxstats -@ 10 host_removal/${sample}/${sample}*.hybrid.sorted.bam | awk -v sample=$sample '{array[$1]=$3; pwet[$1]=$4; next} END {tot=0; unmapped=0; for (chr in array) {tot+=array[chr]; unmapped+=pwet[chr]}; tot+=unmapped; cov=array["MN908947.3"]; hum=tot-cov-unmapped; printf sample"\t"tot"\t"hum"\t%.2f\t"cov"\t%.2f\t"unmapped"\t%.2f\n", 100*hum/tot, 100*cov/tot, 100*unmapped/tot}' >> metrics/host_contamination_metrics.tsv
+    samtools idxstats -@ 10 host_removal/${sample}/${sample}*.host_removed.sorted.bam | awk -v sample=$sample '{array[$1]=$3; pwet[$1]=$4; next} END {tot=0; unmapped=0; for (chr in array) {tot+=array[chr]; unmapped+=pwet[chr]}; tot+=unmapped; cov=array["MN908947.3"]; hum=tot-cov-unmapped; printf sample"\t"tot"\t"hum"\t%.2f\t"cov"\t%.2f\t"unmapped"\t%.2f\n", 100*hum/tot, 100*cov/tot, 100*unmapped/tot}' >> metrics/host_removed_metrics.tsv
 
     echo "$sample,$cons_perc_N,$cons_len,$cons_GC,$cons_genome_frac,$cons_N_perkbp,$fq_surviving_trim,$bam_aln,$bam_surviving_filter,$bam_surviving_primertrim,$bam_meancov,$bam_mediancov,$bam_maxmincovmean,$bam_cov20X,$bam_cov50X,$bam_cov100X,$bam_cov250X,$bam_cov500X,$bam_cov1000X,$bam_cov2000X,$bam_meaninsertsize,$bam_medianinsertsize,$bam_sdinsertsize,$bam_mininsertsize,$bam_maxinsertsize" >> metrics/metrics.csv
     # break
